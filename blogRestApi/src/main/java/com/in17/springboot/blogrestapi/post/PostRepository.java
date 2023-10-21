b39052e2ecf2c5a31410be81374e4a4980705677
Shashank Chourasia <shashankchourasia1706@gmail.com>
2023-10-21 13:46:47 +0530

    delete

--- blogRestApi/src/main/java/com/in17/springboot/blogrestapi/post/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.in17.springboot.blogrestapi.post;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-public interface PostRepository extends JpaRepository<Post, String>{
-
-}
